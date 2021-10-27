7458f6e58363c0b797bd0c6a2b3986e11518df15
William <williamy330@gmail.com>
2021-10-27 12:47:48 -0400

    started tree search algorithm and added naive material eval

--- ChessAI/testing_bot.py
+++ ChessAI/testing_bot.py
@@ -1,6 +1,7 @@
 import chess
 import chess.polyglot
 import random
+import numpy as np
 
 class TestingBot:
     def __init__(self, color):
@@ -20,4 +21,43 @@ class TestingBot:
         moves = list(board.legal_moves)
         return moves[int(len(moves) * random.random())]
 
+    def tree_search(self, board, depth, alpha, beta):
+        if depth == 0 or board.is_game_over():
+            return self.evaluate(board)
+
+        best_value = -1000000
+        for move in board.legal_moves:
+            board.push(move)
+            value = -self.tree_search(board, depth - 1, -beta, -alpha)
+            board.pop()
+            best_value = max(best_value, value)
+            alpha = max(alpha, value)
+            if alpha >= beta:
+                break
+        return best_value
+
+    def evaluate(self, board):
+        if board.is_checkmate():
+            return -1000000 if board.turn == self.color else 1000000
+
+        if board.is_stalemate() or board.is_insufficient_material() or board.is_seventyfive_moves():
+            return 0
+
+        material = 0
+
+        type_pieces = [chess.PAWN, chess.KNIGHT, chess.BISHOP, chess.ROOK, chess.QUEEN, chess.KING]
+        value_pieces = {chess.PAWN: 1, chess.KNIGHT: 3, chess.BISHOP: 3, chess.ROOK: 5, chess.QUEEN: 9, chess.KING: 10000}
+        color_pieces = np.array([chess.WHITE, chess.BLACK])
+        all_pieces = []
+        for type_piece in type_pieces:
+            for color_piece in color_pieces:
+                all_pieces.append((type_piece, color_piece))
+
+        for piece, color in all_pieces:
+            if color == chess.WHITE:
+                material += board.pieces(piece, color) * value_pieces[piece]
+            if color == chess.BLACK:
+                material -= board.pieces(piece, color) * value_pieces[piece]
+
+        return material
         
